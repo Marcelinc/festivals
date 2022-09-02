871959ad8dce3c72c56a103fe93ce4d175c180a6
Marcelinc <marcin.c40@wp.pl>
2022-09-02 11:50:16 +0200

    Lazy loading items on home page

--- src/scripts/homeObserve.js
+++ src/scripts/homeObserve.js
@@ -1,14 +1,101 @@
-import {observer} from './observer'
+//Section one observer
+const optionsOne = {
+    root: null,
+    threshold: 1,   //percent of visible element
+    rootMargin: "-150px"
+}
 
-var sectionOne,sections
+const sectionOneObserver = new IntersectionObserver(function(entries,observer){
+    entries.forEach(entry => {
+        if(!entry.isIntersecting)
+            return
+        entry.target.classList.add('appear')
+        observer.unobserve(entry.target)
 
-document.addEventListener('DOMContentLoaded', () => {
-    sectionOne =  document.getElementById("database")
-    sections = document.querySelectorAll('.homeSection')
-
-    sections.forEach(section => {
-        observer.observe(section)
     })
+},optionsOne)
+
+
+//Section two observer
+const optionsTwo = {
+    root: null,
+    threshold: 0.9,   //percent of visible element
+    rootMargin: "0px 0px -50px 0px"
+}
+const sectionTwoObserver = new IntersectionObserver(function(entries,observer){
+    entries.forEach(entry => {
+        if(!entry.isIntersecting)
+            return
+        entry.target.childNodes.forEach((child,index) => {
+            setTimeout(() => {
+                //console.log(index)
+                child.classList.add('appear')
+                observer.unobserve(entry.target)
+            },index*1000)
+        })
+        
+    })
+},optionsTwo)
+
+
+//Section three (most viewed) observer
+const optionsThree = {
+    root: null,
+    threshold: 1,   //percent of visible element
+    rootMargin: "0px 0px -50px 0px"
+}
+const sectionThreeObserver = new IntersectionObserver(function(entries,observer){
+    entries.forEach((entry,index) => {
+        if(!entry.isIntersecting)
+            return
+
+        setTimeout(() => {
+            entry.target.classList.add('appear')
+            observer.unobserve(entry.target)
+        },index*700)
+        //console.log(entry)
+    })
+},optionsThree)
+
+
+//Section four (form) observer
+const optionsFour = optionsTwo
+const sectionFourObserver = new IntersectionObserver(function(entries,observer){
+    entries.forEach(entry => {
+        console.log(entry)
+        if(!entry.isIntersecting)
+            return
+        
+        console.log(entry.target.tagName)
+        entry.target.classList.add('appear')
+        observer.unobserve(entry.target)
+    })
+},optionsFour)
+
+
+//Initialize observers
+document.addEventListener('DOMContentLoaded', () => {
+    //Section one
+    const sectionOne = document.querySelector('.one').childNodes
+    //console.log(sections)
+    sectionOne.forEach(section => {
+        sectionOneObserver.observe(section)
+    })
+
+    //Section two
+    const sectionTwo = document.querySelector('.two').childNodes
+    //console.log(sectionTwo)
+    sectionTwo.forEach(elem => sectionTwoObserver.observe(elem))
+
+
+    //Section three
+    const sectionMostSearched = document.querySelector('.mostSearched').childNodes
+    sectionMostSearched.forEach(elem => sectionThreeObserver.observe(elem))
+     
+
+    //Section four
+    const sectionFour = document.querySelector('.three').childNodes
+    sectionFour.forEach(elem => sectionFourObserver.observe(elem))
 })
 
 
--- src/scripts/observer.js
+++ src/scripts/observer.js
@@ -1,14 +1,17 @@
 const options = {
     root: null,
-    threshold: .5,   //percent of visible element
-    rootMargin: "0px"
+    threshold: .3,   //percent of visible element
+    rootMargin: "-150px"
 }
 
 export const observer = new IntersectionObserver(function(entries,observer){
     entries.forEach(entry => {
-        if(entry.isIntersecting)
+        if(!entry.isIntersecting)
             return
-        console.log(entry.target)
+        
+        console.log(entry)
+        observer.unobserve(entry.target)
+        
     })
 },options)
 
