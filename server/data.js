4dc0e77ca395a63d1c33a54e5664df08302d86d3
Marcelinc <marcin.c40@wp.pl>
2022-09-12 12:09:56 +0200

    Fixed resolver in FestivalType (returning list of musicTags)

--- server/data.js
+++ server/data.js
@@ -1,7 +1,17 @@
-const festivals = [{id:"1",name:'Tomorrowland',place:{country: 'Belgium', city: 'Boom'}, musicTags: ['EDM', 'House','Hardstyle','Techno'],views: 0},
-{id:"2",name:'Ultra Music Festival', place: {country: 'United States', city: 'Miami,Florida'}, musicTags: ['EDM'],views: 0},
-{id:'3',name:'Sunrise Festival', place: {country: 'Poland', city: 'Kołobrzeg'}, musicTags: ['Big Room','Trance','EDM'],views:0},
-{id:'4',name:'Defqon 1', place: {country: 'Netherlands', city: 'Biddinghuizen'}, musicTags: ['Hardstyle', 'Hard dance','EDM', 'Hardcore', 'Psychedelic Trance'],views:0},
-{id:'5',name:'Parookaville', place: {country: 'Germany', city: 'Weeze'}, musicTags: ['EDM','House'],views:0}]
+const festivals = [{id:"1",name:'Tomorrowland',place:{country: 'Belgium', city: 'Boom'}, musicTags: ["1"],views: 0},
+{id:"2",name:'Ultra Music Festival', place: {country: 'United States', city: 'Miami,Florida'}, musicTags: ['1'],views: 0},
+{id:'3',name:'Sunrise Festival', place: {country: 'Poland', city: 'Kołobrzeg'}, musicTags: ['7','3','1'],views:0},
+{id:'4',name:'Defqon 1', place: {country: 'Netherlands', city: 'Biddinghuizen'}, musicTags: ['4', '8','1', '6', '9'],views:0},
+{id:'5',name:'Parookaville', place: {country: 'Germany', city: 'Weeze'}, musicTags: ['1','2'],views:0}]
 
-module.exports = {festivals}
+const musicGenres = [{id:'1', name: 'EDM'},
+{id: '2', name: 'House'}, 
+{id: '3', name: 'Trance'},
+{id: '4', name: 'Hardstyle'},
+{id: '5', name: 'Techno'},
+{id: '6', name: 'Hardcore'},
+{id: '7', name: 'Big Room'}, 
+{id: '8', name: 'Hard dance'},
+{id: '9', name: 'Psychedelic trance'}]
+
+module.exports = {festivals, musicGenres}
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,5 +1,5 @@
 const { GraphQLObjectType, GraphQLID, GraphQLString, GraphQLList, GraphQLSchema, GraphQLInt } = require("graphql");
-const { festivals } = require("../data");
+const { festivals, musicGenres } = require("../data");
 
 //Place type
 const PlaceType = new GraphQLObjectType({
@@ -10,14 +10,6 @@ const PlaceType = new GraphQLObjectType({
     })
 })
 
-//MusicTags Type
-const MusicTagsType = new GraphQLObjectType({
-    name: 'MusicTags',
-    fields: () => ({
-        musicTags: {type: [String]}
-    })
-})
-
 //Festival Type
 const FestivalType = new GraphQLObjectType({
     name: 'Festival',
@@ -25,21 +17,69 @@ const FestivalType = new GraphQLObjectType({
         id: {type: GraphQLID},
         name: {type: GraphQLString},
         place: {type: PlaceType},
-        //musicTags: {type: MusicTagsType},
+        musicTags: {
+            type: new GraphQLList(MusicGenreType),
+            resolve(parent,args){
+                console.log('parent: ',parent)
+                var tags = []
+                parent.musicTags.forEach(tagID => {
+                    //console.log('tagID: ',tagID)
+                    musicGenres.find(genre => {
+                       // console.log('sprawdzanie ',genre)
+                        if(genre.id === tagID)
+                            tags.push(genre)
+                    })
+                })
+                return tags
+            }
+        },
         views: {type: GraphQLInt}
     })
 })
 
+//MusicGenre Type
+const MusicGenreType = new GraphQLObjectType({
+    name: 'MusicGenre',
+    fields: () => ({
+        id: {type: GraphQLID},
+        name: {type: GraphQLString}
+    })
+})
 
 
 const RootQuery = new GraphQLObjectType({
     name: 'RootQueryType',
     fields: {
+        festivals: {
+            type: new GraphQLList(FestivalType),
+            resolve(parent,args){
+                return festivals
+            }
+        },
         festival: {
             type: FestivalType,
             args: { id: {type: GraphQLID} },
             resolve(parent,args){
                 return festivals.find(festival => festival.id === args.id)
+            }
+        },
+        musicGenres: {
+            type: new GraphQLList(MusicGenreType),
+            resolve(parent,args){
+                return musicGenres
+            }
+        },
+        musicGenre: {
+            type: MusicGenreType,
+            args: {
+                id: {type: GraphQLID},
+                name: {type: GraphQLString}
+            },
+            resolve(parent,args){
+                if(args.id)
+                    return musicGenres.find(genre => genre.id === args.id)
+                if(args.name)
+                    return musicGenres.find(genre => genre.name === args.name)
             }
         }
     }
